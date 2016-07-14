88abbb8405adb8c56712a29d7bfc0f10b7e60a60
craig <couturecraigj@users.noreply.github.com>
2016-07-14 09:44:00 -0400

    Update ApplicationActions.js

--- generators/app/templates/app/actions/ApplicationActions.js
+++ generators/app/templates/app/actions/ApplicationActions.js
@@ -19,7 +19,7 @@ class ApplicationActions {
       if(config.action) {
         return this.alt
         .getActions(config.action)
-        .load()
+        .load(route.params)
         .then(() => {
 
           resolve();
